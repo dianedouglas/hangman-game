209a812c3e249d9ac7b3361f339756292f8a8166
Diane Douglas & Blake Mason <student@epicodus.com>
2014-07-21 16:59:12 -0700

    Add the interface with initial logic to take a word and letter guesses from the user and display empty spaces, wrong letters and the first body part.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var Hangman = {
   initialize: function(word) {
     this.word = word.split("");
-    this.currentBodyPart = 0;
+    this.correctLetters = [];
+    this.wrongLetters = [];
   },
   letterLookUp: function(letter) {
     var letterIndices = [];
@@ -9,16 +10,58 @@ var Hangman = {
       for(var i = 0; i < this.word.length; i++){
         if(this.word[i] === letter){
           letterIndices.push(i);
+          this.correctLetters[i] = letter;
         }
       }
       return letterIndices;
     }
+    this.wrongLetters.push(letter);
     return false;
   },
   isBodyComplete: function() {
-
+    if(this.wrongLetters.length >= 7){
+      return true;
+    }else {
+      return false;
+    }
   },
   isWordComplete: function() {
-
+    var allLettersMatch = true;
+    for (var i=0; i<this.word.length; i++) {
+      if (this.word[i] !== this.correctLetters[i]) {
+        allLettersMatch = false;
+      }
+    }
+    return allLettersMatch;
   }
 };
+
+$(document).ready(function(){
+  var hangmanGame;
+
+  $("#start-word").submit(function(event){
+    event.preventDefault();
+    hangmanGame = Object.create(Hangman);
+    hangmanGame.initialize($('#word').val());
+    $("#start-word").hide();
+    $("#letter-guess").show();
+    hangmanGame.word.forEach(function(eachletter) {
+      $(".correct-letters p").append(" __ ");
+    });
+    $(".hangman-pic").append("<img src='img/90px-Hangman-0.png'>")
+  })
+
+  $("#letter-guess").submit(function(event){
+    event.preventDefault();
+    var userLetter = $("#letter").val();
+    var letterLookUpResult = hangmanGame.letterLookUp(userLetter);
+    if(!letterLookUpResult){
+      $(".wrong-letters p").text(hangmanGame.wrongLetters);
+      $("#letter").val("");
+      $(".hangman-pic").replaceWith("<img src='img/90px-Hangman-"+ hangmanGame.wrongLetters.length +".png'>");
+    } else {
+
+    }
+
+  })
+})
--- spec/specs.js
+++ spec/specs.js
@@ -5,26 +5,76 @@ describe("Hangman", function() {
       newGame.initialize("bob");
       newGame.word.should.eql(["b","o","b"]);
       newGame.currentBodyPart.should.equal(0);
+      newGame.correctLetters.should.eql([]);
+      newGame.wrongLetters.should.eql([]);
     });
   });
   describe("letterLookUp", function() {
     it("take a letter and return character position in given word", function() {
       var newGame = Object.create(Hangman);
       newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
       newGame.letterLookUp("o").should.eql([1]);
+
     });
 
     it("take a letter and return all character positions in given word for duplicates", function() {
       var newGame = Object.create(Hangman);
       newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
       newGame.letterLookUp("b").should.eql([0, 2]);
     });
 
-    it("take a letter and return false if it doesn't exist.", function() {
+    it("take a letter and return all character positions in given word for duplicates and add to correct letters", function() {
       var newGame = Object.create(Hangman);
       newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
+      newGame.letterLookUp("b");
+      newGame.correctLetters.should.eql(["b", , "b"]);
+    });
+
+    it("take a letter and return false if it doesn't exist and add it to wrong letters.", function() {
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
       newGame.letterLookUp("k").should.eql(false);
+      newGame.wrongLetters.should.eql(["k"]);
     });
   });
+  describe("isWordComplete", function(){
+    it("if word is equal to correctLetters return true.", function(){
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
+      newGame.letterLookUp("b");
+      newGame.letterLookUp("o");
+      newGame.isWordComplete().should.equal(true);
+    });
 
+    it("if word is not equal to any in correctLetters return false.", function(){
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.correctLetters = [];
+      newGame.wrongLetters = [];
+      newGame.letterLookUp("b");
+      newGame.isWordComplete().should.equal(false);
+    });
+  });
+  describe("isBodyComplete", function(){
+    it("if the the number of wrong letters exceeds the number of body parts available return true--game over", function(){
+      var newGame = Object.create(Hangman);
+      newGame.wrongLetters = ["a","g","o","b","c","v","h"];
+      newGame.isBodyComplete().should.equal(true);
+    });
+    it("if the the number of wrong letters is fewer than the number of body parts available return false", function(){
+      var newGame = Object.create(Hangman);
+      newGame.wrongLetters = ["o","b","c","v","h"];
+      newGame.isBodyComplete().should.equal(false);
+    });
+  });
 });
