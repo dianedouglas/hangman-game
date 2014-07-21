8a2fd998e0f8a2f723a0d093004794e8e670d529
Diane Douglas & Blake Mason <student@epicodus.com>
2014-07-21 15:58:39 -0700

    Add a function with specs to look up one letter in the given word and return an array of all indices of the letter's position if the letter is present in the word and return false if it is not present.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 var Hangman = {
   initialize: function(word) {
-    this.word = word;
+    this.word = word.split("");
     this.currentBodyPart = 0;
   },
-  letterLookUp: function() {
-
+  letterLookUp: function(letter) {
+    var letterIndices = [];
+    if(this.word.indexOf(letter) >= 0){
+      for(var i = 0; i < this.word.length; i++){
+        if(this.word[i] === letter){
+          letterIndices.push(i);
+        }
+      }
+      return letterIndices;
+    }
+    return false;
   },
   isBodyComplete: function() {
 
--- spec/specs.js
+++ spec/specs.js
@@ -3,12 +3,28 @@ describe("Hangman", function() {
     it("establish a word to play with and variables and methods for body parts and letters in given word", function() {
       var newGame = Object.create(Hangman);
       newGame.initialize("bob");
-      newGame.word.should.equal("bob");
+      newGame.word.should.eql(["b","o","b"]);
       newGame.currentBodyPart.should.equal(0);
-
     });
   });
-  // it("take a letter and return character position in given word", function() {
+  describe("letterLookUp", function() {
+    it("take a letter and return character position in given word", function() {
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.letterLookUp("o").should.eql([1]);
+    });
 
-  // });
+    it("take a letter and return all character positions in given word for duplicates", function() {
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.letterLookUp("b").should.eql([0, 2]);
+    });
+
+    it("take a letter and return false if it doesn't exist.", function() {
+      var newGame = Object.create(Hangman);
+      newGame.word = ["b","o","b"];
+      newGame.letterLookUp("k").should.eql(false);
+    });
+  });
+
 });
